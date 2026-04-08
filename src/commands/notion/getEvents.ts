d330a231dba9ec104fd5c840a0ec9f0552c53c8d
zozzzC <czlc05edu@gmail.com>
2026-04-08 18:50:50 +1200

    enable pagination of events

--- src/commands/notion/getEvents.ts
+++ src/commands/notion/getEvents.ts
@@ -1,6 +1,10 @@
 import { getEvents } from "@/utils/getEvents";
 import {
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
   CommandInteraction,
+  ComponentType,
   EmbedBuilder,
   SlashCommandBuilder,
 } from "discord.js";
@@ -14,8 +18,106 @@ export default {
     ),
   async execute(interaction: CommandInteraction) {
     await interaction.deferReply();
-    await getEvents(false);
-    const embed = new EmbedBuilder().setTitle("Upcoming Events").addFields();
-    await interaction.editReply({ embeds: [embed] });
+    const paginatedEvents = await getEvents(false);
+    const pages: EmbedBuilder[] = [];
+    for (const events of paginatedEvents) {
+      const embed = new EmbedBuilder()
+        .addFields(
+          events.map((e) => {
+            return { name: e.name, value: e.dueDate };
+          }),
+        )
+        .setDescription("Events by date.");
+      pages.push(embed);
+    }
+    let index = 0;
+    let nextPageExists = true;
+
+    if (paginatedEvents.length > 1) {
+      nextPageExists = false;
+    }
+
+    const first = new ButtonBuilder()
+      .setCustomId("first")
+      .setLabel("first")
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(true);
+    const prev = new ButtonBuilder()
+      .setCustomId("prev")
+      .setLabel("prev")
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(true);
+    const next = new ButtonBuilder()
+      .setCustomId("next")
+      .setLabel("next")
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(nextPageExists);
+    const last = new ButtonBuilder()
+      .setCustomId("last")
+      .setLabel("last")
+      .setStyle(ButtonStyle.Secondary)
+      .setDisabled(nextPageExists);
+    const buttons = new ActionRowBuilder<ButtonBuilder>().addComponents([
+      first,
+      prev,
+      next,
+      last,
+    ]);
+
+    const message = await interaction.editReply({
+      embeds: [pages[index]],
+      components: [buttons],
+    });
+    const collector = message.createMessageComponentCollector({
+      componentType: ComponentType.Button,
+      time: 10_000,
+    });
+
+    collector.on("collect", async (i) => {
+      await i.reply({ content: "Processing...", ephemeral: true });
+      await i.deleteReply();
+      if (i.user.id !== interaction.user.id)
+        return await i.reply({
+          content: `You are not authorised to perform this action.`,
+          ephemeral: true,
+        });
+
+      if (i.customId === "first") {
+        index = 0;
+      } else if (i.customId === "prev") {
+        if (index > 0) index--;
+      } else if (i.customId === "next") {
+        if (index < pages.length) index++;
+      } else if (i.customId === "last") {
+        index = pages.length - 1;
+      }
+
+      if (index === 0) {
+        first.setDisabled(true);
+        prev.setDisabled(true);
+      } else {
+        first.setDisabled(false);
+        prev.setDisabled(false);
+      }
+
+      if (index === pages.length - 1) {
+        last.setDisabled(true);
+        next.setDisabled(true);
+      } else {
+        next.setDisabled(false);
+        last.setDisabled(false);
+      }
+
+      await message
+        .edit({ embeds: [pages[index]], components: [buttons] })
+        .catch((err) => {});
+      collector.resetTimer();
+    });
+
+    collector.on("end", async () => {
+      await message.edit({ embeds: [pages[index]], components: [] });
+    });
+
+    return message;
   },
 };
