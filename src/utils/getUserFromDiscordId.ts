9651b56afb34f1b96da1be63231c77ff7f127361
zozzzC <czlc05edu@gmail.com>
2026-04-28 21:21:11 +1200

    lint

--- src/utils/getUserFromDiscordId.ts
+++ src/utils/getUserFromDiscordId.ts
@@ -1,7 +1,6 @@
 import { client } from "@/index";
 
 export default async function getUserFromDiscordId(discordId: string) {
-  console.log(discordId);
   const user = await client.users.fetch(discordId);
   return { execDiscordId: user.id, execDiscordUsername: user.username };
 }
